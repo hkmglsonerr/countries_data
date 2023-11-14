45ca95be6f337f1e23f6c72178417b3caed70c96
unknown <hkmglsonerr@gmail.com>
2023-11-14 10:45:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(countries_data[0].capital);
+console.log(countries_data[0]["capital"]);
+
+for (let i = 0; i < countries_data.length; i++) {
+  console.log(`${countries_data[i].name}`);
+}
+// sum of 0- 100
